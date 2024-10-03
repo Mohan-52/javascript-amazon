c8fda8ba65b1a006bdee4efaafc63d174d8b4260
mohan-52 <mohanaoruganti52@gmail.com>
2024-10-03 09:12:27 +0530

    added to button Selector

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product)=>{
               </div>
 
               <div class="product-quantity-container">
-                <select>
+                <select class="js-quantity-selector-${product.id}">
                   <option selected value="1">1</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
@@ -57,6 +57,9 @@ document.querySelector('.js-products-grid').innerHTML=productHtml;
 
 document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
   button.addEventListener('click',()=>{
+
+    const quantitySelector=document.querySelector(`.js-quantity-selector-${button.dataset.productId}`);
+    const quantity=Number(quantitySelector.value);
    const productId=button.dataset.productId;
 
 
@@ -71,12 +74,12 @@ document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
    });
 
    if(matchingItem){
-    matchingItem.quantity+=1
+    matchingItem.quantity+=quantity;
    }else{
 
    cart.push({
     productId:productId,
-    quantity:1
+    quantity:quantity
    });
 
   }
