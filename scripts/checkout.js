98e0fadc0532a0fbbbc7cb406d0697020abd271e
mohan-52 <mohanaoruganti52@gmail.com>
2024-10-22 19:24:00 +0530

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummerty.js';
 import { renderCheckoutHeader } from './checkout/checkHeader.js';
-
+import  "../data/cart-oop.js";
 
 renderCheckoutHeader();
 renderOrderSummary();
