2081d8cec53839e0dd69c2ff72c06796d5238acb
mohan-52 <mohanaoruganti52@gmail.com>
2024-11-04 17:46:22 +0530

    load products from the backend

--- data/products.js
+++ data/products.js
@@ -96,7 +96,44 @@ console.log(date.toLocaleTimeString()); */
 
 
 
-export const products = [
+
+export let products=[];
+
+
+
+export function loadProducts(fun){
+
+ const xhr= new XMLHttpRequest();
+
+ xhr.addEventListener('load',()=>{
+
+  products=JSON.parse(xhr.response).map((productDetails)=>{
+    if(productDetails.type==='clothing'){
+      return new Clothing(productDetails);
+    }
+    else if(productDetails.type==='appliance'){
+      return new Applicance(productDetails);
+  
+    }
+   return new Product(productDetails);
+
+  });
+
+  console.log('load products');
+
+  fun();
+
+ });
+
+
+
+ xhr.open('GET','https://supersimplebackend.dev/products');
+ xhr.send();
+}
+
+loadProducts();
+
+/* export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -772,4 +809,4 @@ export const products = [
   }
  return new Product(productDetails);
 });
-
+ */
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,12 @@
 import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
-import {products} from '../data/products.js';
+import {products, loadProducts} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
+
+loadProducts(renderProductsGrid);
+
+
+function renderProductsGrid(){
 
 
 
@@ -113,6 +118,8 @@ document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
     
 
 });
+
+}
 
 
 
